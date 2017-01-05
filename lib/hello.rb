f29d2ef1fa9a7da567b2127f13f5fbd46d08559f
zainab amjad <z_amjad@yahoo.com>
2017-01-05 16:58:32 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,8 @@
-def hello_t
-
-end
-
-# call your method here!
-
+def hello_t(names)
+   names.each do |name|
+     if name.start_with?("T")
+        puts "Hi, #{name}"
+      end
+     end
+   end # call your method here! \
+   hello_t(names=["Tim", "Tom", "Jim"])
